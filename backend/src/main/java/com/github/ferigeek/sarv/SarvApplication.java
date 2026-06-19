afd817d1d6802c54971ce141d55812091e604ba8
ferigeek <150931210+ferigeek@users.noreply.github.com>
2026-06-19 17:55:35 +0330

    chore(backend): initialize Spring Boot backend project with dependencies

---
+++ backend/src/main/java/com/github/ferigeek/sarv/SarvApplication.java
@@ -0,0 +1,13 @@
+package com.github.ferigeek.sarv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarvApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/ferigeek/sarv/SarvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ferigeek.sarv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
